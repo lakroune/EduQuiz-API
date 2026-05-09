e9aee4eb6aafde5cf0aefb5075d0278dbf86d393
lakroune <lakroune00@gmail.com>
2026-05-09 23:19:10 +0100

    Add POST endpoint to submit questions and update questions.json with initial data

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 const app = express();
 const PORT = 3000;
-
+const { v4: uuidv4 } = require('uuid');
 app.use(express.json());
 
 const questionsPath = path.join(__dirname, 'data', 'questions.json');
@@ -20,4 +20,27 @@ app.get('/questions', (req, res) => {
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+app.post('/questions', (req, res) => {
+    try {
+        const { question, options, correctAnswer } = req.body;
+
+        const data = fs.readFileSync(questionsPath, 'utf8');
+        const questions = JSON.parse(data);
+
+        const newQuestion = {
+            id: uuidv4(), 
+            question,
+            options,
+            correctAnswer
+        };
+
+        questions.push(newQuestion);
+        fs.writeFileSync(questionsPath, JSON.stringify(questions, null, 2));
+
+        res.status(201).json({ message: "Question added!", question: newQuestion });
+    } catch (err) {
+        res.status(500).json({ message: "Error saving data" });
+    }
 });
